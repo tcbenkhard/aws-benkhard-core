67f08f383be98b8371527a3585d6b4917b8c6bd4
Timo Benkhard <tcbenkhard@gmail.com>
2020-09-23 21:50:50 +0200

    Add volume for Jenkins

--- lib/benkhard-core-stack.ts
+++ lib/benkhard-core-stack.ts
@@ -1,11 +1,12 @@
 import * as cdk from '@aws-cdk/core';
 import * as ecs from '@aws-cdk/aws-ecs';
 import * as ssm from '@aws-cdk/aws-ssm';
-import {Vpc} from "@aws-cdk/aws-ec2";
+import {IVolume, Volume, Vpc} from "@aws-cdk/aws-ec2";
 import * as acm from '@aws-cdk/aws-certificatemanager';
 import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
 import * as route53 from '@aws-cdk/aws-route53';
 import {Duration} from "@aws-cdk/core";
+import * as efs from '@aws-cdk/aws-efs';
 
 export class BenkhardCoreStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -22,11 +23,16 @@ export class BenkhardCoreStack extends cdk.Stack {
     const hostedZoneId = ssm.StringParameter.valueFromLookup(this, '/com/benkhard/public-hosted-zone-id');
     const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'PublicHostedZone', { hostedZoneId: hostedZoneId, zoneName: 'benkhard.com' });
 
+    const fileSystem = new efs.FileSystem(this, 'EfsFileSystem', {
+      vpc,
+      performanceMode: efs.PerformanceMode.GENERAL_PURPOSE,
+    });
+
     const loadBalancerFargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "JenkinsService", {
       cluster: cluster, // Required
       cpu: 256, // Default is 256
       desiredCount: 1, // Default is 1
-      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("jenkins/jenkins"), containerPort: 8080 },
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("jenkins/jenkins"), containerPort: 8080,  },
       memoryLimitMiB: 1024, // Default is 512
       publicLoadBalancer: true, // Default is false
       assignPublicIp: true,
@@ -38,6 +44,22 @@ export class BenkhardCoreStack extends cdk.Stack {
 
     loadBalancerFargateService.targetGroup.configureHealthCheck({
       path: '/login'
+    });
+
+    const efsVolume: ecs.Volume = {
+      name: 'efs',
+      efsVolumeConfiguration: {
+        fileSystemId: fileSystem.fileSystemId,
+        rootDirectory: "/jenkins"
+      }
+    };
+
+    loadBalancerFargateService.taskDefinition.addVolume(efsVolume);
+
+    loadBalancerFargateService.taskDefinition.defaultContainer?.addMountPoints({
+      containerPath: '/var/jenkins_home',
+      readOnly: false,
+      sourceVolume: efsVolume.name
     })
   }
 }
